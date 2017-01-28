071d5be7804d3d25f46dded7e4ddebef653440e3
Surya Dantuluri <surya.mvhs@gmail.com>
2017-01-28 00:25:12 -0800

    updated loops

--- ClassSize.java
+++ ClassSize.java
@@ -24,6 +24,9 @@ public class ClassSize
 	int i;
 	String students;
 	int numberOfStudents;
+	int firstNumber;
+	int secondNumber;
+
 		public ClassSize()																											 //this is the constructor
 		{
 			String[] size = new String[30];
@@ -43,23 +46,69 @@ public class ClassSize
 		Scanner input = new Scanner(System.in);																				//initializes scanner input
 		System.out.println("\n\n\nWelcome to ClassSize. Please enter your class size. (Enter a positive integer, such as 39)");
 		sum = 0;
-		for(int i = 0; i < size.length(); i++)
-		{
-				System.out.print("\n");
-				students = input.nextLine();
-				students = students.trim();
-				numberOfStudents = Double.parseDouble(students);
-				sum = sum + numberOfStudents;
-				size[i] = studnets;
 
-		}
+		for(int i = 0; i < 30; i++)
+			{
+			System.out.print("\n");
+			students = input.nextLine();
+			students = students.trim();
+			students = Double.parseDouble(students);
+			numberOfStudents = Double.parseDouble(students);
+			sum = sum + numberOfStudents;
+			size[i] = students;
+			}
 	}
 
 	public void printData()
 	{
-		System.out.prtinf("There are %f total number of classrooms", size.length);
-		System.out.prtinf("The average class size is %f students", sum/size.length);
-		System.out.prtinf("The average class size is %f students", sum/size.length);
+		for(int i = 0; i < 30; i++)
+			{
+			System.out.println("Class size of inputted room %f: %s\n\n", i, size[i]);
+			}
+
+		for(int i = 0; i < 30; i++)
+			{
+				firstNumber = size[i];
+				secondNumber = 1 + size[i];
+				maxValue = Math.max(firstNumber, secondNumber);
+
+				if (size[i] > maxValue)
+				{
+        maxValue = convertedValues[i];
+    		}
+			}
+
+			for(int i = 0; i < 30; i++)
+				{
+					firstNumber = size[i];
+					secondNumber = 1 + size[i];
+					minValue = Math.min(firstNumber, secondNumber);
+
+					if (size[i] < minValue)
+					{
+	        minValue = convertedValues[i];
+	    		}
+				}
+
+
+		System.out.prtinf("There are %f total number of classrooms\n", size.length);
+		System.out.prtinf("The average number of students in a class is %f students\n", sum/(size.length));
+		System.out.prtinf("The biggest class has %f students", maxValue);
+		System.out.prtinf("The smallest class has %f students", minValue);
+		System.out.prtinf("These are the classes that exceeed the ideal maximum of students in a classroom (exceed 36 students)";
+
+		for(int i = 0; i < 30; i++)
+			{
+				if(size[i]>36)
+				{
+				
+				System.out.println
+				}
+
+
+			}
+
+
 
 	}
 
