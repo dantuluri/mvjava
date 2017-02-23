d6b984e87abb2786e5d902ae9430557ce1e22a1f
Surya Dantuluri <surya.mvhs@gmail.com>
2017-02-23 10:33:26 -0800

    another quiz with framework

---
+++ GraphicsQuiz1.java
@@ -0,0 +1,69 @@
+/*
+* Surya Dantuluri
+* February 8,2017
+*  Masterpiece.java
+* Simple program that finds out slope and y-intercept depending on inputted equation
+* Uses printf() and format, if and else, do while loops
+* Uses double, integer, character, and float.
+* Uses Scanner class
+*
+* initializes all needed variables
+* input equation
+* output trimmed equation, and form of line
+* output slope and y-intercept
+*/
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+    public class masterpieceframe extends JFrame
+    {
+
+        public static void main (String[]args)
+        {
+            masterpieceframe mpf = new masterpieceframe();
+        }
+
+        public masterpieceframe()
+        {
+        super("masterpieceframe");
+        setSize(900,900);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(0,0);
+        setResizable(true);
+
+        Panel pan = new Panel();
+        setContentPane(pan);
+        setVisible(true);
+        }
+
+
+    class Panel extends JPanel
+    {
+
+        public Panel()
+        {
+            setBackground(Color.BLUE);
+        }
+
+        public void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);
+            
+            g.fillArc(20,20,80,120,60,60);
+            g.drawLine(60,80,60,120);
+            g.drawRect(40,120,40,60);
+            for (int y=160; y<=170; y+=10){
+            for (int x = 40; x <=70; x+=10){
+            g.drawOval( x, y, 10, 10);
+            }}
+
+
+
+
+        }
+
+
+
+    }
+}
