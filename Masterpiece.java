9dcf503ed98ab3791a5f21fe4103d7865cf63514
Surya Dantuluri <surya.mvhs@gmail.com>
2017-02-24 13:03:57 -0800

    no pseudo code basically done no refinements

--- Masterpiece.java
+++ Masterpiece.java
@@ -52,11 +52,17 @@ import javax.swing.*;
 
             g.drawOval(600,150,100, 200); //draws oval with x and y changing to draw repetitively
             g.drawRect(100,100,450,300); //draws rectangle with x and y changing to draw repetitively
-            g.fillArc(375,600,150,110,0,180);
+            g.fillArc(275,600,150,110,0,180);
+            g.drawLine(100,100,550,400);
+            Font serifBold50 = new Font("Serif",Font.BOLD,50);
+            g.setFont(serifBold50);
+            g.drawString("Yipee!",100,350);
+            g.drawArc(0,450,200,100,0,180);
 
-            Font serifBold20 = new Font("Serif",Font.BOLD,20);
-            g.setFont(serifBold20);
-            g.drawString("Yipee!",100,250);
+            int [] xpolygon = {200, 400, 600, 350,200}; //array for x values for the left bottom polygon
+    			  int [] ypolygon = {450, 450, 550, 700,700}; //array for y values for the left bottom polygon
+    			  g.drawPolygon(xpolygon, ypolygon, 5); //polygon with x,y array values to draw polygon
+
 
     //COLOR purplo = new Color(255,0,255);
 
