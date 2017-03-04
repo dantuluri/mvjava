4a788c055adc487186b91c89c294fa16ca01ee9d
Surya Dantuluri <surya.mvhs@gmail.com>
2017-03-03 23:28:34 -0800

    didn't update latest garden

--- GardenGrows.java
+++ GardenGrows.java
@@ -1,82 +1,150 @@
-//Surya Dantuluri March 3rd Update
-public class GardenGrows extends JApplet{
-	public void init(){
-		Garden fsp = new Garden();
-		setContentPane(fsp);
+/*
+* Surya Dantuluri
+* March 2, 2017
+* GardenGrows.java
+* Simple program that displays a repeated pattern of shapes *on a fabric, which is then covered by polygons to display a *picture of pajamas on a panel.
+* Uses JFrame class and JPanel container.
+* Uses integers and arrays.
+*
+* Class first declares extending JFrame class.
+* Then sets size, sets size of origin of content panel, sets * if window is resizable or not.
+*
+* Sets new panel then sets background
+* Sets pattern, covering all of the panel
+* Overlays pattern with other shapes in panel to make the
+* output look like pajamas.
+*
+*Testing Plan:
+*input: no input
+*output:
+*/
+import java.awt.Color;     //imports abstract window toolkit
+import java.awt.Graphics; //imports possible events of abstract window toolkit
+import java.awt.Image; //imports graphics components with swing
+import java.awt.Font;
+import java.awt.event.MouseListener;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+
+public class GardenGrows
+{
+	private Garden canvas;
+	private Image image;
+	private JLabel label;
+	private int xpos, ypos;
+	private boolean keyClear;
+	private int sizeX, sizeY;
+	private String imageName;
+	private boolean shiftkey;
+
+	public GardenGrows()
+	{
+		sizeX=217;
+		sizeY=301;
+		imageName="calvin.jpg";
+		xpos=ypos=200;
+		keyClear=true;
 	}
-}
-class Garden extends JPanel implements MouseListener, KeyListener{ //implement listeners
-	boolean mod;
-	boolean up;
-	boolean water;
-	boolean sun;
-	int x;
-	int y;
-
-	public Garden(){
-		mod = false;
-		up = false;
-		water = false;
-		sun = false;
-		addMouseListener(this); //add "ears" to Panel//add listeners in constructor
-		addKeyListener(this);
+	public static void main(String[]args)
+	{
+		GardenGrows garden = new GardenGrows();
+		garden.run();
 	}
 
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
+	public void getMyImage()
+	{
+		try
+		{
+			image=ImageIO.read(new File(imageName));
+		}
+		catch(IOException e)
+		{
+		System.err.println("\n\n"+imageName+"can't be found. \n\n");
+		e.printStackTrace();
+		}
+	}
 
-		g.drawString("To add water, click on the pink rectangle and type %. To add sun, click on the rectangle and press the up key.",30,30);
+	public void run()
+	{
+		JFrame frame = new JFrame("Garden Grows");
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setLayout(null); //get ContentRaw().setLayout(null);
+		frame.getContentPane();
+		canvas=new Garden();
+		frame.getContentPane().add(canvas);
+		getMyImage();
+		frame.setBackground(Color.PINK);
+		frame.setSize(1000,1000);
+		frame.setLocation(0,0);
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
 
-		g.setColor(Color.PINK);
-		g.fillRect(50,50,100,50);//create 100x50 pink rectangle with corner in 50,50
-
-		if(x < 150 && x > 50 && y < 100 && y > 50 && mod == true){//if x1, y1 are within rect and % is true
-			water = true; //return true
-			g.setColor(Color.GREEN);  //turn garden green
-			g.fillRect(50,50,100,50);
+	class Garden extends JPanel implements MouseListener,KeyListener
+	{
+		public Garden()
+		{
+			setBackground(Color.PINK);
+			setLocation(50,50);
+			setSize(1000,500);
+			addMouseListener(this);
+			addKeyListener(this);
 		}
 
-		if(x < 150 && x > 50 && y < 100 && y > 50 && up == true){
-			sun = true;
+		public void mousePressed(MouseEvent e)
+		{
+			requestFocus();
+			repaint();
 		}
 
+		public void mouseClicked(MouseEvent e){}
+		public void mouseReleased(MouseEvent e){}
+		public void mouseEntered(MouseEvent e){}
+		public void mouseExited(MouseEvent e){}
 
-		if(water == true && sun == true){//if x1, y1, x2, y2 coordinates are within rect and %, up arrow is true
-			g.setColor(Color.BLACK);
-			for(int f = 50; f < 145; f+=20){//loop for flowers: 5x5 filled circles, flowers appear every 20 pixels in right, left, up, down in garden
-				for(int j = 50; j <= 95; j+=20){
-				g.fillOval(f,j,5,5);
-				}
+		public void keyPressed(KeyEvent e)
+		{
+			int code = e.getKeyCode();
+			if(code==KeyEvent.VK_SHIFT)
+			{
+				shiftkey=true;
+			}
+			if(shiftkey&&code==KeyEvent.VK_5)
+			{
+			setBackground(Color.GREEN);
+			}
+			if(code==KeyEvent.VK_SPACE)
+			{
+				keyClear=true;
+				repaint();
 			}
 		}
-	}
 
-	public void keyPressed(KeyEvent e){} //keyPressed method
-	public void keyReleased(KeyEvent e){
-		if(e.getKeyCode() == KeyEvent.VK_UP){//if input char is up
-			up = true; //return true
-			repaint();
-		}
-	}
+		public void keyTyped(KeyEvent e){}
+		public void keyReleased(KeyEvent e){}
 
+		public void paintComponent(Graphics g)
+		{
+			if(keyClear)
+			{
+				super.paintComponent(g);
+			}
+			else
+			{
+				g.drawImage(image,xpos,ypos,sizeX,sizeY,this);
+			}
+			keyClear=false;
 
-	public void keyTyped(KeyEvent e){//keyTyped method
-		if(e.getKeyChar() == '%'){//if input char is %
-			mod = true; //return true
-			repaint();
-		}
-
-	}
-
-	public void mousePressed(MouseEvent e){} //5 mouse methods
-	public void mouseReleased(MouseEvent e){}
-	public void mouseClicked(MouseEvent e){
-		requestFocus();
-		x = e.getX();
-		y = e.getY();//get coordinates of click, save as x, y
 		}
 
-	public void mouseEntered(MouseEvent e){}
-	public void mouseExited(MouseEvent e){}
+	}
 
 }
