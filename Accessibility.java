13bed7c618559bdbd444fdb9e3c18036c65ba201
Surya Dantuluri <surya.mvhs@gmail.com>
2017-03-07 19:52:13 -0800

    updated accessibility INITIAL COMMIT

---
+++ Accessibility.java
@@ -0,0 +1,20 @@
+/*
+* Surya Dantuluri
+* March 2, 2017
+* Accessibility.java
+* Simple program that displays a repeated pattern of shapes *on a fabric, which is then covered by polygons to display a *picture of pajamas on a panel.
+* Uses JFrame class and JPanel container.
+* Uses integers and arrays.
+*
+* Class first declares extending JFrame class.
+* Then sets size, sets size of origin of content panel, sets * if window is resizable or not.
+*
+* Sets new panel then sets background
+* Sets pattern, covering all of the panel
+* Overlays pattern with other shapes in panel to make the
+* output look like pajamas.
+*
+*Testing Plan:
+*input: no input
+*output:
+*/
