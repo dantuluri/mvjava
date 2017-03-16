647f67e75f29f5f900b632855d0d82fdcf60dea6
Surya Dantuluri <surya.mvhs@gmail.com>
2017-03-15 22:57:26 -0700

    update my nanme: still need to get from squkmail

--- SimpleLayout.java
+++ SimpleLayout.java
@@ -1,8 +1,9 @@
-/*	
+/*
+Surya Dantuluri
 	March  2017
 	SimpleLayout.java
 	This program provides examples of:
-	
+
 	1. BorderLayout
 	2. GridLayout
 	3. FlowLayout
@@ -12,7 +13,7 @@
 import javax.swing.JFrame;	//import javax.swing.*;
 import javax.swing.JPanel;
 
-import java.awt.event.MouseListener;	// or import java.awt.event.*;	
+import java.awt.event.MouseListener;	// or import java.awt.event.*;
 import java.awt.event.MouseEvent;
 
 import java.awt.Color;		//import java.awt.*;
@@ -24,53 +25,53 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.CardLayout;
 import java.awt.FlowLayout;
-public class SimpleLayout 
+public class SimpleLayout
 {
 	JFrame borderFrame, gridFrame, flowFrame, cardFrame;
-	
-	public static void main(String[] args) 
+
+	public static void main(String[] args)
 	{
 		SimpleLayout sl = new SimpleLayout();
 		sl.run();
 	}
-	
-	public void run() 
+
+	public void run()
 	{
 		makeBorderLayout();		// BorderLayout window
 		makeGridLayout();		// GridLayout window
 		makeFlowLayout();		// FlowLayout window
 		makeCardLayout();		// CardLayout window
 	}
-	
-	public void makeBorderLayout() 
+
+	public void makeBorderLayout()
 	{
 		MyPanel panBL1, panBL2, panBL3, panBL4, panBL5;
 		MyColorPanel colorPanel;
 		// Create a JFrame with BorderLayout
 		borderFrame = new JFrame("BorderLayout");	// Create the JFrame
-		
+
 		/////////////////////////////////////////////////
 		// Set the layout to BorderLayout (default for JFrame)
 		//
 		borderFrame.setLayout(new BorderLayout());
-		
+
 		borderFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		borderFrame.setSize(300, 300);
 		borderFrame.setLocation(10, 10);
-		
+
 		// Create panels
 		panBL1 = new MyPanel(1, Color.RED);  panBL1.setPreferredSize(new Dimension(50, 30));
 		panBL2 = new MyPanel(2, Color.BLUE);  panBL2.setPreferredSize(new Dimension(50, 20));
 		panBL3 = new MyPanel(3, Color.GREEN);  panBL3.setPreferredSize(new Dimension(50, 30));
 		panBL4 = new MyPanel(4, Color.ORANGE);  panBL4.setPreferredSize(new Dimension(50, 20));
 		panBL5 = new MyPanel(5, Color.MAGENTA);  panBL5.setPreferredSize(new Dimension(50, 20));
-		
+
 		/////////////////////////////////////////////////
 		// Initialize colorPanel (part 5)
 		//
 		colorPanel=new MyColorPanel();
-		
-		
+
+
 		/////////////////////////////////////////////////
 		// Add panels to the frame
 		//
@@ -80,25 +81,25 @@ public class SimpleLayout
 		borderFrame.getContentPane().add(panBL3,BorderLayout.SOUTH);
 		borderFrame.getContentPane().add(panBL4,BorderLayout.EAST);
 		borderFrame.getContentPane().add(panBL3,BorderLayout.WEST);
-				
+
 		// Make the JFrame visible
 		borderFrame.setVisible(true);
 	}
-	
-	public void makeGridLayout() 
+
+	public void makeGridLayout()
 	{
 		MyPanel panGL1, panGL2, panGL3, panGL4, panGL5, panGL6;
 		// Create a JFrame with BorderLayout
 		gridFrame = new JFrame("GridLayout");	// Create the JFrame
-		
+
 		/////////////////////////////////////////////////
 		// Set the layout to GridLayout
 		gridFrame.setLayout(new GridLayout(3,2));
-				
+
 		gridFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gridFrame.setSize(300, 300);
 		gridFrame.setLocation(210, 120);
-		
+
 		// Create panels
 		panGL1 = new MyPanel(1, Color.RED);
 		panGL2 = new MyPanel(2, Color.BLUE);
@@ -106,7 +107,7 @@ public class SimpleLayout
 		panGL4 = new MyPanel(4, Color.ORANGE);
 		panGL5 = new MyPanel(5, Color.MAGENTA);
 		panGL6 = new MyPanel(6, Color.YELLOW);
-		
+
 		// Add panels to the frame
 		gridFrame.getContentPane().add(panGL1);
 		gridFrame.getContentPane().add(panGL2);
@@ -114,28 +115,28 @@ public class SimpleLayout
 		gridFrame.getContentPane().add(panGL4);
 		gridFrame.getContentPane().add(panGL5);
 		gridFrame.getContentPane().add(panGL6);
-		
+
 		// Make the JFrame visible
 		gridFrame.setVisible(true);
 	}
-	
-	public void makeFlowLayout() 
+
+	public void makeFlowLayout()
 	{
 		MyPanel panFL1, panFL2, panFL3, panFL4, panFL5, panFL6;
 		// Create a JFrame with BorderLayout
 		flowFrame = new JFrame("FlowLayout");	// Create the JFrame
-		
+
 		/////////////////////////////////////////////////
 		// Set the layout to FlowLayout
 		flowFrame.setLayout(new FlowLayout());
 		//
-		
-		
+
+
 		// notice this has DISPOSE.  What is the effect of that?
-		flowFrame.setDefaultCloseOperation(flowFrame.DISPOSE_ON_CLOSE);	
+		flowFrame.setDefaultCloseOperation(flowFrame.DISPOSE_ON_CLOSE);
 		flowFrame.setSize(300, 300);
 		flowFrame.setLocation(310, 220);
-		
+
 		// Initialize panels
 		panFL1 = new MyPanel(1, Color.RED);
 		panFL2 = new MyPanel(2, Color.BLUE);
@@ -160,43 +161,43 @@ public class SimpleLayout
 		flowFrame.getContentPane().add(panFL4);
 		flowFrame.getContentPane().add(panFL5);
 		flowFrame.getContentPane().add(panFL6);
-		
+
 		// Make the JFrame visible
 		flowFrame.setVisible(true);
 	}
-	
-	public void makeCardLayout() 
+
+	public void makeCardLayout()
 	{
 		MyColorPanel colorPanel;
 		// Create a JFrame with BorderLayout
-		
+
 		cardFrame = new JFrame("CardLayout");	// Create the JFrame
 		cardFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		cardFrame.setSize(300, 300);
 		cardFrame.setLocation(410, 320);
-		
+
 		// Create panels
 		colorPanel = new MyColorPanel();
-		
+
 		// Add panels to the frame
 		cardFrame.getContentPane().add(colorPanel);
-		
+
 		// Make the JFrame visible
 		cardFrame.setVisible(true);
 	}
 }	// end SimpleLayout
 
-// a simple panel with number and background color. 
-class MyPanel extends JPanel 
+// a simple panel with number and background color.
+class MyPanel extends JPanel
 {
 	private String num;
 	private Color color;
-	public MyPanel(int numIn, Color colorIn) 
+	public MyPanel(int numIn, Color colorIn)
 	{
 		num = "" + numIn;
 		setBackground(colorIn);
 	}
-	public void paintComponent(Graphics g) 
+	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
 		g.setFont(new Font("Arial", Font.BOLD, 18));
@@ -204,22 +205,22 @@ class MyPanel extends JPanel
 		g.drawString(num, 10, 20);
 	}
 }	// end class MyPanel
-	
-class MyColorPanel extends JPanel implements MouseListener 
+
+class MyColorPanel extends JPanel implements MouseListener
 {
 	//////////////////////////////////////////////////////////
 	// Declare CardLayout and JPanel objects
 	//
 	private CardLayout cards;
 	private JPanel color1, color2, color3;
-	
+
 	public MyColorPanel() 			// constructor
 	{	////////////////////////////////////////////////////////
 		// Initialize CardLayout and setLayout() to this panel
 		//
 		cards = new CardLayout();
 		setLayout(cards);
-		
+
 		// Create blue, green and red JPanels and set their backgrounds
 		color1 = new MyPanel(1, Color.BLUE);
 		color2 = new MyPanel(2, Color.GREEN);
@@ -227,7 +228,7 @@ class MyColorPanel extends JPanel implements MouseListener
 
 		////////////////////////////////////////////////////////
 		// Add the MouseListener to *each* JPanel and add
-		
+
 		color1.addMouseListener(this);
 		color2.addMouseListener(this);
 		color3.addMouseListener(this);
@@ -238,7 +239,7 @@ class MyColorPanel extends JPanel implements MouseListener
 		int counter = 0;
 		//
 	}
-	public void mousePressed(MouseEvent evt) 
+	public void mousePressed(MouseEvent evt)
 	{
 		////////////////////////////////////////////////////////
 		// Select the next panel on the stack of panels
