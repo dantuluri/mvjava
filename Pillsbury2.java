5e38ad002c8def2defa2e2d219f7a43f5e63904e
Surya Dantuluri <surya.mvhs@gmail.com>
2017-03-22 17:45:23 -0700

    homework stress and whiriligigigigigigigigigggigigigigigigigigigiigigigigigigigi

--- Pillsbury2.java
+++ Pillsbury2.java
@@ -1,21 +1,21 @@
 // JC
 // 2017
-// Pillsbury.java  
+// Pillsbury.java
 // A panel in which a button is used to make the panel giggle.
-// Pillsbury Dough Boy. poke his belly and he giggled.  
+// Pillsbury Dough Boy. poke his belly and he giggled.
 // User interacts with a button labeled "press my tummy."  Upon pressing, "tee hee" is drawn
-// and the button gets relabeled "reset."  Upon pressing, screen refreshes, 
+// and the button gets relabeled "reset."  Upon pressing, screen refreshes,
 // meaning the text goes away.
 
 // 1.  create an ActionListener class
 // 2.  create a button to print a string when pressed.
 // 3.  print string when button is pressed
 
-import java.awt.Color;	
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Font;
 
-import javax.swing.JFrame;	
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 
@@ -26,105 +26,91 @@ import java.awt.event.ActionEvent;
 
 
 public class Pillsbury2
-{	
-	public static void main( String[] args )
-	{
-		Pillsbury2 pills2 = new Pillsbury2();
-		pills2.runIt();
-	}
-	
-	public Pillsbury2()
-	{
-	}
-	
-	public void runIt()
-	{
-		JFrame frame = new JFrame("Pillsbury Dough boy");
-		frame.setSize( 400, 300);				
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
-		frame.setLocation(400,50);
-		frame.setResizable(true);
-		PillsburyPanel2 pillsPanel2 = new PillsburyPanel2(); 		
-		frame.getContentPane().add( pillsPanel2 );	
-		frame.setVisible(true);		
-	}
-
+{
+    public static void main( String[] args )
+    {
+        Pillsbury2 pills2 = new Pillsbury2();
+        pills2.runIt();
+    }
+    
+    public Pillsbury2()
+    {
+    }
+    
+    public void runIt()
+    {
+        JFrame frame = new JFrame("Pillsbury Dough boy");
+        frame.setSize( 400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocation(400,50);
+        frame.setResizable(true);
+        PillsburyPanel2 pillsPanel2 = new PillsburyPanel2();
+        frame.getContentPane().add( pillsPanel2 );
+        frame.setVisible(true);
+    }
+}
 
 ////////////////////////////// class with ActionListener  /////////////////
-// 1.  
-		class PillsburyPanel2 extends JPanel implements ActionListener
-		{
-			private boolean pressed;	// used to tell is button pressed
-			private JButton button;
-		////////////// write constructor
-			public PillsburyPanel2()
-			{
-				pressed=false;
-				button=new JButton("Press my belly.");
-				button.addActionListener(this);
-				add(button);
-			
-			}
-		//////
-		// 2.  instantiate the button.  add everything needed.
-
-	
-	public void paintComponent(Graphics g)	// paint component
-	{
-		super.paintComponent (g);	// draw background		
-		Font font = new Font ("Serif", Font.BOLD, 30);
-		g.setFont( font );	
-		
-		if(pressed)
-		{
-			g.drawString("tee hee",10,100);
-			pressed=false;
-		}
-		
-	}
-			
-		public void actionPerformed(ActionEvent evt)
-		{
-			/*String command=button.getText();
-			if(command.equals("Press my belly."))
-			{
-				pressed=true;
-				button.setText("reset");
-			} 
-			
-			else
-			{
-				button.setText("Press my belly.");
-			}
-			repaint();
-			*/
-
-		}
-
-	class ButtonHandler implements ActionListener
-	{
-		public ButtonHandler()
-		{
-			
-		}
-		
-		public void actionPerformed(ActionEvent evt)
-		{
-			String command=button.getText();
-			if(command.equals("Press my belly."))
-			{
-				pressed=true;
-				button.setText("reset");
-			} 
-			
-			else
-			{
-				button.setText("Press my belly.");
-			}
-			repaint();
-		}
-	}
-
-	}
+// 1.
+class PillsburyPanel2 extends JPanel
+{
+    private boolean pressed;	// used to tell is button pressed
+    private JButton button;
+    ////////////// write constructor
+    public PillsburyPanel2()
+    {
+        pressed=false;
+        button=new JButton("Press my belly");
+        ButtonHandler buttonhandle = new ButtonHandler();
+        button.addActionListener(buttonhandle);
+        add(button);
+    }
+    //////
+    // 2.  instantiate the button.  add everything needed.
+    
+    
+    public void paintComponent(Graphics g)	// paint component
+    {
+        super.paintComponent (g);	// draw background
+        Font font = new Font ("Serif", Font.BOLD, 30);
+        g.setFont( font );
+        
+        if(pressed)
+        {
+            g.drawString("tee hee",10,100);
+            pressed=false;
+        }
+        
+    }
+    class ButtonHandler implements ActionListener
+    {
+        
+        public ButtonHandler()
+        {
+            
+        }
+        
+        public void actionPerformed(ActionEvent evt)
+        {
+            String command1=evt.getActionCommand();
+            
+            if(command1.equals("Press my belly"))
+            {
+                pressed=true;
+                button.setText("reset");
+            } 
+            
+            else
+            {
+                button.setText("Press my belly");
+            }
+            repaint();
+        }
+    }
+    
+    
 }
+
+
+
 
---
+++ Whirligig.java
@@ -0,0 +1,170 @@
+/*	
+	March  2017
+	SimpleLayout.java
+	This program provides examples of:
+	
+	1. BorderLayout
+	2. GridLayout
+	3. FlowLayout
+	4. CardLayout
+	5. Nested layouts
+*/
+import javax.swing.JFrame;	//import javax.swing.*;
+import javax.swing.JPanel;
+
+import java.awt.event.MouseListener;	// or import java.awt.event.*;	
+import java.awt.event.MouseEvent;
+
+import java.awt.Color;		//import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.Dimension;
+///////////////////////// import Classes needed for Layouts ////////////////////////
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+public class Whirligig 
+{
+	JFrame flowFrame;
+	
+	public static void main(String[] args) 
+	{
+		Whirligig gig = new Whirligig();
+		gig.run();
+	}
+	
+	public void run() 
+	{
+		makeFlowLayout();		// FlowLayout window
+	}
+	
+	
+	public void makeFlowLayout() 
+	{
+		MyPanel panFL1, panFL2;
+		// Create a JFrame with BorderLayout
+		flowFrame = new JFrame("FlowLayout");	// Create the JFrame
+		
+		/////////////////////////////////////////////////
+		// Set the layout to FlowLayout
+		flowFrame.setLayout(new FlowLayout());
+		//
+		
+		
+		// notice this has DISPOSE.  What is the effect of that?
+		flowFrame.setDefaultCloseOperation(flowFrame.DISPOSE_ON_CLOSE);	
+		flowFrame.setSize(1400, 1100);
+		flowFrame.setLocation(0, 0);
+		
+		// Initialize panels
+		panFL1 = new MyPanel(1, Color.RED);
+		panFL2 = new MyPanel(2, Color.BLUE);
+
+		/////////////////////////////////////////////////
+		// setPreferredSize for all panels - 1st we will run the prog with out this.
+		//
+		panFL1.setPreferredSize(new Dimension(450,400));
+		panFL2.setPreferredSize(new Dimension(450,400));
+		
+
+		flowFrame.getContentPane().add(panFL1);
+		flowFrame.getContentPane().add(panFL2);
+	
+		
+		// Make the JFrame visible
+		flowFrame.setVisible(true);
+	}
+	
+
+// a simple panel with number and background color. 
+class MyPanel extends JPanel implements ActionListener 
+{
+	private String num;
+	private Color color;
+	private boolean pressed;	// used to tell is button pressed
+			private JButton button;
+	public MyPanel(int numIn, Color colorIn) 		//numbers on the panel
+	{
+		JRadioButton radiobutton = new JRadioButton("hello");
+			radiobutton.addActionListener(this);
+				add(radiobutton); 
+				JRadioButton radiobutton2 = new JRadioButton("adios");
+			radiobutton.addActionListener(this);
+				add(radiobutton2); 
+				JRadioButton radiobutton3 = new JRadioButton("hsdfsfello");
+			radiobutton.addActionListener(this);
+				add(radiobutton3); 
+				JRadioButton radiobutton4 = new JRadioButton("hetghhfghllo");
+			radiobutton.addActionListener(this);
+				add(radiobutton4); 
+	}
+	public void paintComponent(Graphics g) 
+	{
+		super.paintComponent(g);
+		g.setFont(new Font("Arial", Font.BOLD, 18));
+		g.setColor(Color.RED);
+		g.drawString(num, 10, 20);
+	}
+	
+	public void actionPerformed(ActionEvent evt)
+		{
+			
+
+	}
+}	// end class MyPanel
+	
+class MyColorPanel extends JPanel implements MouseListener 
+{
+	//////////////////////////////////////////////////////////
+	// Declare CardLayout and JPanel objects
+	//
+	private CardLayout cards;
+	private JPanel color1, color2, color3;
+	
+	public MyColorPanel() 			// constructor
+	{	////////////////////////////////////////////////////////
+		// Initialize CardLayout and setLayout() to this panel
+		//
+		cards = new CardLayout();
+		setLayout(cards);
+		
+		// Create blue, green and red JPanels and set their backgrounds
+		color1 = new MyPanel(1, Color.BLUE);
+		color2 = new MyPanel(2, Color.GREEN);
+		color3 = new MyPanel(3, Color.RED);
+
+		////////////////////////////////////////////////////////
+		// Add the MouseListener to *each* JPanel and add
+		
+		//color1.addMouseListener(this);
+		//color2.addMouseListener(this);
+		//color3.addMouseListener(this);
+		// each JPanel to MyColorPanel (this)
+		add(color1,"Panel 1");
+		add(color2,"Panel 2");
+		add(color3,"Panel 3");
+		int counter = 0;
+		//
+	}
+	public void mousePressed(MouseEvent evt) 
+	{
+		////////////////////////////////////////////////////////
+		// Select the next panel on the stack of panels
+		//cards.next(this);
+		//
+
+	}
+	public void mouseEntered(MouseEvent evt) { }
+	public void mouseExited(MouseEvent evt) { }
+	public void mouseClicked(MouseEvent evt) { }
+	public void mouseReleased(MouseEvent evt) { }
+}
+
+
+}
