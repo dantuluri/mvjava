121eeab0ef234066f21f3c7945cc861f1dbe42a2
Surya Dantuluri <surya.mvhs@gmail.com>
2017-03-27 13:16:35 -0700

    updtatoes

--- Whirligig.java
+++ Whirligig.java
@@ -1,6 +1,6 @@
 /*
 	March  2017
-	SimpleLayout.java
+	Whiligig.java
 	This program provides examples of:
 
 	1. BorderLayout
@@ -9,14 +9,10 @@
 	4. CardLayout
 	5. Nested layouts
 */
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 import javax.swing.JFrame;	//import javax.swing.*;
 import javax.swing.JPanel;
 
-import java.awt.event.MouseListener;	// or import java.awt.event.*;
-import java.awt.event.MouseEvent;
+//import java.awt.event.MouseEvent;
 
 import java.awt.Color;		//import java.awt.*;
 import java.awt.Graphics;
@@ -32,25 +28,15 @@ import javax.swing.JRadioButton;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
+
 public class Whirligig
 {
 	JFrame flowFrame;
-
-	public static void main(String[] args)
+	public Whirligig()
 	{
-		Whirligig gig = new Whirligig();
-		gig.run();
-	}
-
-	public void run()
-	{
-		makeFlowLayout();		// FlowLayout window
-	}
-
-
-	public void makeFlowLayout()
-	{
-		MyPanel panFL1, panFL2;
+		RadioProbability panFL1;
+		ScrollRisk panFL2;
 		// Create a JFrame with BorderLayout
 		flowFrame = new JFrame("FlowLayout");	// Create the JFrame
 
@@ -62,12 +48,12 @@ public class Whirligig
 
 		// notice this has DISPOSE.  What is the effect of that?
 		flowFrame.setDefaultCloseOperation(flowFrame.DISPOSE_ON_CLOSE);
-		flowFrame.setSize(140, 110);
+		flowFrame.setSize(1400, 1100);
 		flowFrame.setLocation(0, 0);
 
 		// Initialize panels
-		panFL1 = new MyPanel(1, Color.RED);
-		panFL2 = new MyPanel(2, Color.BLUE);
+		panFL1 = new RadioProbability(1, Color.RED);
+		panFL2 = new ScrollRisk(2, Color.BLUE);
 
 		/////////////////////////////////////////////////
 		// setPreferredSize for all panels - 1st we will run the prog with out this.
@@ -84,35 +70,106 @@ public class Whirligig
 		flowFrame.setVisible(true);
 	}
 
+	public static void main(String[] args)
+	{
+		System.out.println("I will remember to log out.");
+		Whirligig gig = new Whirligig();
+		//gig.run();
+	}
 
-// a simple panel with number and background color.
-class MyPanel extends JPanel implements ActionListener
+	//public void run()
+	//{
+		//makeFlowLayout();		// FlowLayout window
+	//}
+
+
+	//public void makeFlowLayout()
+	//{
+
+	//}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class RadioProbability extends JPanel implements ActionListener
 {
 	private String num;
 	private Color color;
 	private boolean pressed;	// used to tell is button pressed
-			private JButton button;
-	public MyPanel(int numIn, Color colorIn) 		//numbers on the panel
+	private JButton button;
+	public RadioProbability(int numIn, Color colorIn) 		//numbers on the panel
 	{
-		JRadioButton radiobutton = new JRadioButton("hello");
+		JRadioButton radiobutton = new JRadioButton("1");
 			radiobutton.addActionListener(this);
 				add(radiobutton);
-				JRadioButton radiobutton2 = new JRadioButton("adios");
-			radiobutton.addActionListener(this);
+				JRadioButton radiobutton2 = new JRadioButton("2");
+			radiobutton2.addActionListener(this);
 				add(radiobutton2);
-				JRadioButton radiobutton3 = new JRadioButton("hsdfsfello");
-			radiobutton.addActionListener(this);
+				JRadioButton radiobutton3 = new JRadioButton("3");
+			radiobutton3.addActionListener(this);
 				add(radiobutton3);
-				JRadioButton radiobutton4 = new JRadioButton("hetghhfghllo");
-			radiobutton.addActionListener(this);
+				JRadioButton radiobutton4 = new JRadioButton("4");
+			radiobutton4.addActionListener(this);
 				add(radiobutton4);
+
 	}
 	public void paintComponent(Graphics g)
 	{
-		super.paintComponent(g);
-		g.setFont(new Font("Arial", Font.BOLD, 18));
-		g.setColor(Color.RED);
-		g.drawString(num, 10, 20);
+		//super.paintComponent(g);
+		//g.setFont(new Font("Arial", Font.BOLD, 18));
+		//g.setColor(Color.RED);
+		//g.drawString(num, 10, 20);
+	}
+
+	public void actionPerformed(ActionEvent evt)
+		{
+			String command=button.getText();
+			if(command.equals("Press my belly."))
+			{
+				pressed=true;
+				button.setText("reset");
+			}
+
+			else
+			{
+				button.setText("Press my belly.");
+			}
+			repaint();
+
+	}
+}	// end class MyPanel
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class ScrollRisk extends JPanel implements ActionListener
+{
+	private String num;
+	private Color color;
+	private boolean pressed;	// used to tell is button pressed
+	private JButton button;
+	public ScrollRisk(int numIn, Color colorIn) 		//numbers on the panel
+	{
+		JRadioButton radiobutton = new JRadioButton("1");
+			radiobutton.addActionListener(this);
+				add(radiobutton);
+				JRadioButton radiobutton2 = new JRadioButton("2");
+			radiobutton2.addActionListener(this);
+				add(radiobutton2);
+				JRadioButton radiobutton3 = new JRadioButton("3");
+			radiobutton3.addActionListener(this);
+				add(radiobutton3);
+				JRadioButton radiobutton4 = new JRadioButton("4");
+			radiobutton4.addActionListener(this);
+				add(radiobutton4);
+				if(radiobutton.isSelected())
+				{
+				System.out.println("it works woot");
+				}
+	}
+	public void paintComponent(Graphics g)
+	{
+		//super.paintComponent(g);
+		//g.setFont(new Font("Arial", Font.BOLD, 18));
+		//g.setColor(Color.RED);
+		//g.drawString(num, 10, 20);
 	}
 
 	public void actionPerformed(ActionEvent evt)
@@ -120,117 +177,9 @@ class MyPanel extends JPanel implements ActionListener
 
 
 	}
-}	// end class MyPanel
-
-class MyColorPanel extends JPanel implements MouseListener
-{
-	//////////////////////////////////////////////////////////
-	// Declare CardLayout and JPanel objects
-	//
-	private CardLayout cards;
-	private JPanel color1, color2, color3;
-
-	public MyColorPanel() 			// constructor
-	{	////////////////////////////////////////////////////////
-		// Initialize CardLayout and setLayout() to this panel
-		//
-		cards = new CardLayout();
-		setLayout(cards);
-
-		// Create blue, green and red JPanels and set their backgrounds
-		color1 = new MyPanel(1, Color.BLUE);
-		color2 = new MyPanel(2, Color.GREEN);
-		color3 = new MyPanel(3, Color.RED);
-
-		////////////////////////////////////////////////////////
-		// Add the MouseListener to *each* JPanel and add
-
-		//color1.addMouseListener(this);
-		//color2.addMouseListener(this);
-		//color3.addMouseListener(this);
-		// each JPanel to MyColorPanel (this)
-		add(color1,"Panel 1");
-		add(color2,"Panel 2");
-		add(color3,"Panel 3");
-		int counter = 0;
-		//
-	}
-	public void mousePressed(MouseEvent evt)
-	{
-		////////////////////////////////////////////////////////
-		// Select the next panel on the stack of panels
-		//cards.next(this);
-		//
-
-	}
-	public void mouseEntered(MouseEvent evt) { }
-	public void mouseExited(MouseEvent evt) { }
-	public void mouseClicked(MouseEvent evt) { }
-	public void mouseReleased(MouseEvent evt) { }
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-public class ArcControl extends JApplet implements AdjustmentListener
-{
-	private DisplayPanel canvas;
-	private JScrollBar width;
-	private JScrollBar height;
-	private JScrollBar startDeg;
-	private JScrollBar endDeg;
-	int widthof, heightof, startDegof, endDegof;
-
-
-	public ArcControl ( )
-	{
-		widthof = heightof = startDegof = endDegof = 60;
-	}
-
-	public void init()
-	{
-		canvas = new DisplayPanel();
-		getContentPane().add ( canvas, BorderLayout.CENTER );
-
-		width = new JScrollBar(JScrollBar.HORIZONTAL, 60, 5, 0, 400);
-		width.addAdjustmentListener(this);
-		getContentPane().add ( width, BorderLayout.NORTH );
-
-		height = new JScrollBar(JScrollBar.VERTICAL, 60, 5, 0, 400);
-		height.addAdjustmentListener(this);
-		getContentPane().add ( height, BorderLayout.EAST );
-
-		startDeg = new JScrollBar(JScrollBar.VERTICAL, 60, 5, 0, 360);
-		startDeg.addAdjustmentListener(this);
-		getContentPane().add ( startDeg, BorderLayout.WEST );
-
-		endDeg = new JScrollBar(JScrollBar.HORIZONTAL, 60, 5, 0, 370);
-		endDeg.addAdjustmentListener(this);
-		getContentPane().add ( endDeg, BorderLayout.SOUTH );
-	}
-
-	class DisplayPanel extends JPanel
-	{
-
-		public DisplayPanel ( )
-		{
-			setBackground ( Color.black );
-		}
-
-		public void paintComponent(Graphics g )
-		{
-			super.paintComponent ( g );
-			g.setColor(Color.blue);
-			g.fillArc(30, 30, widthof, heightof, startDegof, endDegof);
-		}
-	}
-
-	public void adjustmentValueChanged( AdjustmentEvent e)
-	{
-        	widthof = width.getValue();
-		heightof = height.getValue();
-		startDegof = startDeg.getValue();
-		endDegof = endDeg.getValue();
-		canvas.repaint();
-	}
-}
 
 
 
