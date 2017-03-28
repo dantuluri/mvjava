3bf1f46c9e5af1f4951dd2c341fa8eb81b53b1f1
Surya Dantuluri <surya.mvhs@gmail.com>
2017-03-28 08:02:06 -0700

    done with layout 1

--- Whirligig.java
+++ Whirligig.java
@@ -46,6 +46,9 @@ public class Whirligig
 		// Set the layout to FlowLayout
 		flowFrame.setLayout(new FlowLayout());
 		//
+		panFL1 = new RadioProbability(1, Color.RED);
+		//panFL2 = new ScrollRisk(2, Color.BLUE);
+
 
 
 		// notice this has DISPOSE.  What is the effect of that?
@@ -54,18 +57,16 @@ public class Whirligig
 		flowFrame.setLocation(0, 0);
 
 		// Initialize panels
-		panFL1 = new RadioProbability(1, Color.RED);
-		panFL2 = new ScrollRisk(2, Color.BLUE);
 
 		/////////////////////////////////////////////////
 		// setPreferredSize for all panels - 1st we will run the prog with out this.
 		//
 		panFL1.setPreferredSize(new Dimension(450,400));
-		panFL2.setPreferredSize(new Dimension(450,400));
+		//panFL2.setPreferredSize(new Dimension(450,400));
 
 
 		flowFrame.getContentPane().add(panFL1);
-		flowFrame.getContentPane().add(panFL2);
+	//	flowFrame.getContentPane().add(panFL2);
 
 
 		// Make the JFrame visible
@@ -92,6 +93,7 @@ public class Whirligig
 }
 class RadioProbability extends JPanel implements ActionListener
 {
+
 	private String num;
 	private Color color;
 	private boolean pressed;	// used to tell is button pressed
@@ -117,58 +119,6 @@ class RadioProbability extends JPanel implements ActionListener
 	 group.add(radiobutton3);
 	 group.add(radiobutton4);
 	}
-<<<<<<< HEAD
-
-	public void makeFlowLayout()
-	{
-		RadioProbability panFL1;
-		//
-		// // Create a JFrame with BorderLayout
-		// flowFrame = new JFrame("FlowLayout");	// Create the JFrame
-		//
-		// /////////////////////////////////////////////////
-		// // Set the layout to FlowLayout
-		// flowFrame.setLayout(new FlowLayout());
-		// //
-		//
-		//
-		// // notice this has DISPOSE.  What is the effect of that?
-		// flowFrame.setDefaultCloseOperation(flowFrame.DISPOSE_ON_CLOSE);
-		// flowFrame.setSize(300, 300);
-		// flowFrame.setLocation(310, 220);
-		//
-		// // Initialize panels
-		//
-		// panFL6 = new MyPanel(6, Color.YELLOW);
-		//
-		// /////////////////////////////////////////////////
-		// // setPreferredSize for all panels - 1st we will run the prog with out this.
-		// //
-		// panFL1.setPreferredSize(new Dimension(40,100));
-		// panFL2.setPreferredSize(new Dimension(50,200));
-		// panFL3.setPreferredSize(new Dimension(60,300));
-		// panFL4.setPreferredSize(new Dimension(70,100));
-		// panFL5.setPreferredSize(new Dimension(40,300));
-		// panFL6.setPreferredSize(new Dimension(40,300));
-		//
-		// flowFrame.getContentPane().add(panFL1);
-		// flowFrame.getContentPane().add(panFL2);
-		// flowFrame.getContentPane().add(panFL3);
-		// flowFrame.getContentPane().add(panFL4);
-		// flowFrame.getContentPane().add(panFL5);
-		// flowFrame.getContentPane().add(panFL6);
-		//
-		// // Make the JFrame visible
-		// flowFrame.setVisible(true);
-	}
-
-
-
-
-
-
-=======
->>>>>>> parent of 44844ef... done
 	public void paintComponent(Graphics g)
 	{
 		//super.paintComponent(g);
@@ -192,34 +142,13 @@ class RadioProbability extends JPanel implements ActionListener
 	}
 }	// end class MyPanel
 
-class ScrollRisk extends JPanel implements ActionListener
+class ScrollRisk extends JPanel
+
 {
-	private String num;
-	private Color color;
-	private boolean pressed;	// used to tell is button pressed
-	private JButton button;
-	public ScrollRisk(int numIn, Color colorIn) 		//numbers on the panel
-	{
-		JRadioButton radiobutton = new JRadioButton("1");
-		radiobutton.addActionListener(this);
-		add(radiobutton);
-		JRadioButton radiobutton2 = new JRadioButton("2");
-		radiobutton2.addActionListener(this);
-		add(radiobutton2);
-		JRadioButton radiobutton3 = new JRadioButton("3");
-		radiobutton3.addActionListener(this);
-		add(radiobutton3);
-		JRadioButton radiobutton4 = new JRadioButton("4");
-		radiobutton4.addActionListener(this);
-		add(radiobutton4);
-		if(radiobutton.isSelected())
-		{
-			System.out.println("it works woot");
-		}
-	}
-<<<<<<< HEAD
+
 	// public void makeGridLayout()
 	// {
+	// 	MyPanel panGL1, panGL2, panGL3, panGL4, panGL5, panGL6;
 	// 	// Create a JFrame with BorderLayout
 	// 	gridFrame = new JFrame("GridLayout");	// Create the JFrame
 	//
@@ -246,22 +175,7 @@ class ScrollRisk extends JPanel implements ActionListener
 	// 	gridFrame.getContentPane().add(panGL4);
 	// 	gridFrame.getContentPane().add(panGL5);
 	// 	gridFrame.getContentPane().add(panGL6);
-	//
-	// 	// Make the JFrame visible
-	// 	gridFrame.setVisible(true);
-	// }
-=======
-	public void paintComponent(Graphics g)
-	{
-		//super.paintComponent(g);
-		//g.setFont(new Font("Arial", Font.BOLD, 18));
-		//g.setColor(Color.RED);
-		//g.drawString(num, 10, 20);
-	}
-	public void actionPerformed(ActionEvent evt)
-	{
 
-
-	}
->>>>>>> parent of 44844ef... done
+		// Make the JFrame visible
+	//	gridFrame.setVisible(true);
 }
