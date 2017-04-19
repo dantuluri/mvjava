11d80b79c305468b474d3761fdc0b888ef0299ef
Surya Dantuluri <surya.mvhs@gmail.com>
2017-04-19 07:52:48 -0700

    added initial pseudocode

---
+++ 1Digestive and Metabolism Game/Game.java
@@ -0,0 +1,22 @@
+/*Surya Dantuluri
+ * April 17th 2017
+ * 
+ * Digestive System Game
+ * 
+ * Welcome screen starts and button is under title
+ * 
+ * Human, health bar and foods are present. A drawing on the human pops up trying to signify that s(he) should  feed the human food.
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+public class Game {
+	
+	public static void main (String args[]) {
+		
+	}
+}
+
