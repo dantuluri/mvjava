e3c626a08fc00fa532c0fc0cc81889210bc452e6
Surya Dantuluri <surya.mvhs@gmail.com>
2017-04-19 08:59:56 -0700

    edited some images and pseudocode

--- 1Digestive and Metabolism Game/Game.java
+++ 1Digestive and Metabolism Game/Game.java
@@ -1,22 +1,26 @@
 /*Surya Dantuluri
  * April 17th 2017
- * 
+ *
  * Digestive System Game
- * 
+ *
  * Welcome screen starts and button is under title
- * 
+ *
  * Human, health bar and foods are present. A drawing on the human pops up trying to signify that s(he) should  feed the human food.
- * 
- * 
- * 
- * 
+ *
+ * Should have 6 different classes signifying each of the stages: Mouth, Esophogous, Stomach, Duodenum, Small intestine, *Large intestine
+ *
+ *-------
+ * Game starts off with welcome screen.
+ * Human points to the food options on the right
+ * Health bar is shown.
+ * Virtual human player urges normal human player to eat food by showing food on the right.
+ *
  */
 
 
 public class Game {
-	
+
 	public static void main (String args[]) {
-		
+
 	}
 }
-
