76569a8d5a9106b006855bfe19d34c13d8d8b8dc
Surya Dantuluri <surya.mvhs@gmail.com>
2017-04-19 22:36:51 -0700

    added develop

--- 1Digestive and Metabolism Game/Game.java
+++ 1Digestive and Metabolism Game/Game.java
@@ -17,10 +17,131 @@
  *
  */
 
+import javax.swing.JFrame;	//import javax.swing.*;
+import javax.swing.JPanel;
 
-public class Game {
+//import java.awt.event.MouseEvent;
 
-	public static void main (String args[]) {
+import java.awt.Color;		//import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.Dimension;
+///////////////////////// import Classes needed for Layouts ////////////////////////
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
 
-	}
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
+
+public class Game
+{
+    JFrame frame;
+    public Game()
+    {
+        landingPage landPage;
+        healthPanel panBar;
+        // Create a JFrame with BorderLayout
+        frame = new JFrame("FlowLayout");	// Create the JFrame
+        
+        // notice this has DISPOSE.  What is the effect of that?
+        frame.setDefaultCloseOperation(flowFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.setLocation(0, 0);
+        
+        // Initialize panels
+        //send stuff to  panBar = new RadioProbability(1, Color.RED);
+        //send stuff to panFL2 = new ScrollRisk(2, Color.BLUE);
+        
+        /////////////////////////////////////////////////
+        // setPreferredSize for all panels - 1st we will run the prog with out this.
+        //
+        landPage.setPreferredSize(new Dimension(800,800));
+        panBar.setPreferredSize(new Dimension(220,70));
+        
+        
+        frame.getContentPane().add(landPage);
+        frame.getContentPane().add(panBar);
+        
+        
+        // Make the JFrame visible
+        frame.setVisible(true);
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println("I will remember to log out.");
+        Whirligig gig = new Whirligig();
+        //gig.run();
+    }
+    
+    //public void run()
+    //{
+    //makeFlowLayout();		// FlowLayout window
+    //}
+    
+    
+    //public void makeFlowLayout()
+    //{
+    
+    //}
+    
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    class RadioProbability extends JPanel implements ActionListener
+    {
+        private String num;
+        private Color color;
+        private boolean pressed;	// used to tell is button pressed
+        private JButton button;
+        public RadioProbability(int numIn, Color colorIn) 		//numbers on the panel
+        {
+            JRadioButton radiobutton = new JRadioButton("1");
+            radiobutton.addActionListener(this);
+            add(radiobutton);
+            JRadioButton radiobutton2 = new JRadioButton("2");
+            radiobutton2.addActionListener(this);
+            add(radiobutton2);
+            JRadioButton radiobutton3 = new JRadioButton("3");
+            radiobutton3.addActionListener(this);
+            add(radiobutton3);
+            JRadioButton radiobutton4 = new JRadioButton("4");
+            radiobutton4.addActionListener(this);
+            add(radiobutton4);
+            
+        }
+        public void paintComponent(Graphics g)
+        {
+            //super.paintComponent(g);
+            //g.setFont(new Font("Arial", Font.BOLD, 18));
+            //g.setColor(Color.RED);
+            //g.drawString(num, 10, 20);
+        }
+        
+        public void actionPerformed(ActionEvent evt)
+        {
+            String command=button.getText();
+            if(command.equals("Press my belly."))
+            {
+                pressed=true;
+                button.setText("reset");
+            } 
+            
+            else
+            {
+                button.setText("Press my belly.");
+            }
+            repaint();
+            
+        }
+    }	// end class MyPanel
+    
+    
 }
+
+
