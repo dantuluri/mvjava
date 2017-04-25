eb8ace48d76c2e650e89235a7403b8134c75a42b
Surya Dantuluri <surya.mvhs@gmail.com>
2017-04-24 19:54:09 -0700

    work work work work work

--- 1EatHealthy/EatHealthy.java
+++ 1EatHealthy/EatHealthy.java
@@ -17,47 +17,62 @@
  *
  */
 
- import java.awt.Graphics;//add graphics
- import java.awt.Graphics2D;
- import java.awt.Image;
- import java.awt.Toolkit;//add toolkit to move images around
- import java.awt.event.MouseEvent;
- import java.awt.event.MouseMotionListener;
-import javax.swing.JFrame;	//import javax.swing.*;
-import javax.swing.JPanel;//add jpanel
-
-//import java.awt.event.MouseEvent;
-
-import java.awt.Color;		//import java.awt.*;
-import java.awt.Graphics;
-import java.awt.Font;
-import java.awt.Dimension;
+// import javax.swing.JFrame;	//import javax.swing.*;
+// import javax.swing.JPanel;
+//
+// //import java.awt.event.MouseEvent;
+//
+// import java.awt.Color;		//import java.awt.*;
+// import java.awt.Font;
+// import java.awt.Dimension;
 ///////////////////////// import Classes needed for Layouts ////////////////////////
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.CardLayout;
-import java.awt.FlowLayout;
-import javax.swing.JButton;
-import javax.swing.JRadioButton;
+// import java.awt.BorderLayout;
+// import java.awt.GridLayout;
+// import java.awt.CardLayout;
+// import java.awt.FlowLayout;
+// import javax.swing.JButton;
+// import javax.swing.JRadioButton;
 
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JCheckBox;
 
-public class EatHealthy
+public class EatHealthy extends JFrame
 {
-    JFrame frame;
-    public EatHealthy()
-    {
-        landingPage landPage;
-        healthPanel panBar;
+      private PanelHolder pHolder;
+            private CardLayout cards;
+            private HomePage homePan;//welcome screen
+            private LandingPage landingPan;//where the user is most of the time
+            private HealthBarPanel healthyBar;//health bar panel in LandingPage
+            private FoodPanel foodyBar;//list of images regarding food in LandingPage
+            //Mouth, Esophogous, Stomach, Duodenum, Small intestine, *Large intestine
+            private MouthPanel mouthProcess;
+            private MouthQuestions mouthQuiz;
+            private EsophogousPanel esophogousProcess;
+            private EsophogousQuestions esophogousQuiz;
+            private StomachPanel stomachProcess;
+            private StomachQuestions stomachQuiz;
+            private SmallIntestinePanel smallIntestineProcess;
+            private SmallIntestineQuestions smallintestineQuiz;
+            private LargeIntestine esophogousProcess;
+            private LargeIntestine esophogousQuiz;
+
+
         // Create a JFrame with BorderLayout
         frame = new JFrame("FlowLayout");	// Create the JFrame
 
         // notice this has DISPOSE.  What is the effect of that?
         frame.setDefaultCloseOperation(flowFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(800, 800);//set size of frame
-        frame.setLocation(0, 0);//set location of frmae
+        frame.setSize(800, 800);
+        frame.setLocation(0, 0);
 
         // Initialize panels
         //send stuff to  panBar = new RadioProbability(1, Color.RED);
@@ -102,20 +117,20 @@ public class EatHealthy
         public Mouth(int numIn, Color colorIn) 		//numbers on the panel
         {
             JRadioButton radiobutton = new JRadioButton("1");
-            radiobutton.addActionListener(this);//set radio button of mouth panel to add quiz questions
+            radiobutton.addActionListener(this);
             add(radiobutton);
-            JRadioButton radiobutton2 = new JRadioButton("2");//quiz questions
+            JRadioButton radiobutton2 = new JRadioButton("2");
             radiobutton2.addActionListener(this);
             add(radiobutton2);
-            JRadioButton radiobutton3 = new JRadioButton("3");//quiz questions
+            JRadioButton radiobutton3 = new JRadioButton("3");
             radiobutton3.addActionListener(this);
             add(radiobutton3);
-            JRadioButton radiobutton4 = new JRadioButton("4");//quiz quistions
+            JRadioButton radiobutton4 = new JRadioButton("4");
             radiobutton4.addActionListener(this);
             add(radiobutton4);
 
         }
-        public void paintComponent(Graphics g)//paintComponent of Mouth Class
+        public void paintComponent(Graphics g)
         {
             //super.paintComponent(g);
             //g.setFont(new Font("Arial", Font.BOLD, 18));
@@ -142,136 +157,4 @@ public class EatHealthy
     }	// end class Mouth
 
 
-}
-
-class Stomach extends JComponent implements MouseMotionListener
-{
-
-// drag jcomponent to move image
-  static int imageWidth = 60, imageHeight = 60;
-  int imageX, imageY;
-
-  Image image;
-
-  public Esophogous(Image i) {
-    image = i;//include image
-    addMouseMotionListener(this);
-  }
-
-  public void mouseDragged(MouseEvent e) {
-    imageX = e.getX();
-    imageY = e.getY();
-    repaint();
-  }
-
-  public void mouseMoved(MouseEvent e) {
-  }
-
-  public void paint(Graphics g) {
-    Graphics2D g2 = (Graphics2D) g;
-
-    g2.drawImage(image, imageX, imageY, this);
-  }
-
-  public static void main(String[] args) {
-    String imageFile = "A.jpg";
-    // Turn off double buffering
-    RepaintManager.currentManager(null).setDoubleBufferingEnabled(false);
-
-    Image image = Toolkit.getDefaultToolkit().getImage(DragImage.class.getResource(imageFile));//include toolkit to drag images around
-    image = image.getScaledInstance(imageWidth, imageHeight, Image.SCALE_DEFAULT);//include width and height of the image
-    JFrame frame = new JFrame("DragImage");//make new fram but CHANGE TO PANEL
-    frame.getContentPane().add(new DragImage(image));
-    frame.setSize(300, 300);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);
-  }
-}
-
-class SmallIntestine extends JComponent implements MouseMotionListener
-{
-
-// drag jcomponent to move image
-  static int imageWidth = 60, imageHeight = 60;
-  int imageX, imageY;
-
-  Image image;
-
-  public SmallIntestine(Image i) {
-    image = i;
-    addMouseMotionListener(this);
-  }
-
-  public void mouseDragged(MouseEvent e) {
-    imageX = e.getX();
-    imageY = e.getY();
-    repaint();
-  }
-
-  public void mouseMoved(MouseEvent e) {
-  }
-
-  public void paint(Graphics g) {
-    Graphics2D g2 = (Graphics2D) g;
-
-    g2.drawImage(image, imageX, imageY, this);
-  }
-
-  public static void main(String[] args) {
-    String imageFile = "A.jpg";
-    // Turn off double buffering
-    RepaintManager.currentManager(null).setDoubleBufferingEnabled(false);
-
-    Image image = Toolkit.getDefaultToolkit().getImage(DragImage.class.getResource(imageFile));//include toolkit to drag images around
-    image = image.getScaledInstance(imageWidth, imageHeight, Image.SCALE_DEFAULT);//include width and height of the image
-    JFrame frame = new JFrame("DragImage");//make new fram but CHANGE TO PANEL
-    frame.getContentPane().add(new DragImage(image));
-    frame.setSize(300, 300);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);
-  }
-}
-
-class LargeIntestine extends JComponent implements MouseMotionListener
-{
-
-// drag jcomponent to move image
-  static int imageWidth = 60, imageHeight = 60;
-  int imageX, imageY;
-
-  Image image;
-
-  public LargeIntestine(Image i) {
-    image = i;
-    addMouseMotionListener(this);
-  }
-
-  public void mouseDragged(MouseEvent e) {
-    imageX = e.getX();
-    imageY = e.getY();
-    repaint();
-  }
-
-  public void mouseMoved(MouseEvent e) {
-  }
-
-  public void paint(Graphics g) {
-    Graphics2D g2 = (Graphics2D) g;
-
-    g2.drawImage(image, imageX, imageY, this);
-  }
-
-  public static void main(String[] args) {
-    String imageFile = "A.jpg";
-    // Turn off double buffering
-    RepaintManager.currentManager(null).setDoubleBufferingEnabled(false);
-
-    Image image = Toolkit.getDefaultToolkit().getImage(DragImage.class.getResource(imageFile));//include toolkit to drag images around
-    image = image.getScaledInstance(imageWidth, imageHeight, Image.SCALE_DEFAULT);//include width and height of the image
-    JFrame frame = new JFrame("DragImage");//make new fram but CHANGE TO PANEL
-    frame.getContentPane().add(new DragImage(image));
-    frame.setSize(300, 300);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);
-  }
 }
