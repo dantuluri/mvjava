69b586fd6d27f7f4b45cdb84d05769d7f9389603
Surya Dantuluri <surya.mvhs@gmail.com>
2017-05-17 20:49:57 -0700

    amy

--- 1EatHealthy/EatHealthy.java
+++ 1EatHealthy/EatHealthy.java
@@ -894,7 +894,7 @@ class LandingPage extends JPanel implements MouseMotionListener, MouseListener//
           public Image oatmeal;
           public Image soda;
           public Image avocado;
-
+          public Image amylase;
           public String whatKey;
           public String keychange;
 
@@ -907,6 +907,7 @@ class LandingPage extends JPanel implements MouseMotionListener, MouseListener//
           public String avocadoName;//file name
           public String sodaName;//file name
           public String oatmealName;//file name
+          public String amylaseName;
           public int donutX, donutY, oatmealX, oatmealY, sodaX, sodaY, avocadoX, avocadoY;
           public boolean[] keys = new boolean[4];
 
@@ -960,6 +961,7 @@ class LandingPage extends JPanel implements MouseMotionListener, MouseListener//
             avocadoName= ("avocado.png");//file name
             sodaName= ("soda.png");//file name
             oatmealName= ("oatmeal.png");//file name
+            amylaseName= ("amylase.png");
             digestiveSystem = new String[5];
             whatKey="";
 
@@ -1035,6 +1037,9 @@ class LandingPage extends JPanel implements MouseMotionListener, MouseListener//
 
               oatmeal = ImageIO.read(new File(oatmealName));
               System.out.println("buffered oatmeal");
+
+              amylase = ImageIO.read(new File(amylaseName));
+              System.out.println("buffered amylase");
 
             }
             catch(IOException e)
@@ -1240,11 +1245,26 @@ class LandingPage extends JPanel implements MouseMotionListener, MouseListener//
             public void paintComponent(Graphics g)
             {
               super.paintComponent(g);
+               int enzymeX1 = (int )(Math.random() * 383 + 230);
+               int enzymeY1 = (int )(Math.random() * 439 + 396);
+               int enzymeX2 = (int )(Math.random() * 383 + 230);
+               int enzymeY2 = (int )(Math.random() * 439 + 396);
+               int enzymeX3 = (int )(Math.random() * 383 + 230);
+               int enzymeY3 = (int )(Math.random() * 439 + 396);
+               int enzymeX4 = (int )(Math.random() * 383 + 230);
+               int enzymeY4 = (int )(Math.random() * 439 + 396);
+               int enzymeX5 = (int )(Math.random() * 383 + 230);
+               int enzymeY5 = (int )(Math.random() * 439 + 396);
+               int enzymeX6 = (int )(Math.random() * 383 + 230);
+               int enzymeY6 = (int )(Math.random() * 439 + 396);
+               int enzymeX7 = (int )(Math.random() * 383 + 230);
+               int enzymeY7 = (int )(Math.random() * 439 + 396);
 
               if(inMouth)
               {
                 System.out.println("It IS IN THE MOTUHaisdjoaijd[asodjsoapidjasoidjasoidjsadadas]");
                 g.drawImage(mouthBackground,0,0,800,800,this);
+                g.drawImage(amylase,enzymeX1,enzymeY1,15,22,this);
                 if(alreadyInit)
                 {
                   donutX = 20;//donut-(600 to 750 ,75 to 225) ORIGINAL POS
